731a107cfbe08900135f991b5f526d51cb57550f
tilek881 <tilekt881@gmail.com>
2024-11-16 21:22:36 +0600

    game

---
+++ main.py
@@ -0,0 +1,15 @@
+from logic import play_game
+from decouple import config
+
+
+def main():
+        min_number = config("min_number", cast=int)
+        max_number = config("max_number", cast=int)
+        attempts = config("attempts", cast=int)
+        start_capital = config("start_capital", cast=int)
+
+        play_game(min_number, max_number, attempts, start_capital)
+
+
+if __name__ == "__main__":
+    main()
